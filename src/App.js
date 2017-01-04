5598ca799c43bfb059a79c57754c18f41fd86c04
Matthew Mack <matthew.y.p.mack@gmail.com>
2017-01-04 15:11:22 +1100

    added body component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // Components
 import Header from './header';
 import Footer from './footer';
+import Body from './body';
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
         <Header />
+        <Body />
         <Footer />
       </div>
     );
