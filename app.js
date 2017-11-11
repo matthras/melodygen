f518f037cc396af026afbd837680d0eea046fdea
matthras <matthew.y.p.mack@gmail.com>
2017-11-12 10:04:12 +1100

    switched static path to client/public (as created by CRA)

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'client/public')))
 
 app.use('/', index);
 app.use('/users', users);
