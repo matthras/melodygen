416a5f43d5a24504c839c37cb1f45cf1beeb4983
matthras <matthew.y.p.mack@gmail.com>
2017-11-21 11:12:26 +1100

    added functioning VexFlow example

--- client/src/components/MusicScore.js
+++ client/src/components/MusicScore.js
@@ -1,49 +1,51 @@
 // Component to display the music score
 import React, { Component } from 'react';
-import Vex from 'vexflow';
 import { Accidental } from 'vexflow/src/accidental';
 import { Stave } from 'vexflow/src/stave';
 import { StaveNote } from 'vexflow/src/stavenote';
 import { Voice } from 'vexflow/src/voice';
 import { Formatter } from 'vexflow/src/formatter';
-import { ReactNativeSVGContext, NotoFontPack } from 'standalone-vexflow-context';
+import { Renderer } from 'vexflow/src/renderer';
 
 class MusicScore extends Component {
   // Receives encoded music through props.
   // Renders the actual music onto the page based on stuff passed down from props.
   // Q: How do I make this work with that music score library?
-  constructor(props) {
-    super(props);
-  }
-  runVexFlowCode(context) {
-    const stave = new stave(100, 150, 200);
-    stave.setContext(context);
-    stave.setClef('treble');
-    stave.setTimeSignature('4/4');
-    stave.setText('VexFlow on React!',3);
-    stave.draw();
 
-    const notes = [
-      new StaveNote({clef: "treble", keys: ["c/4", "e/4"], duration: "q" })
-        .addAccidental(0, new Accidental("##")).addDotToAll(),
+  componentDidMount() {
+    const div = document.getElementById("musicScore");
+    let renderer = new Renderer(div, 3);
+    renderer.resize(500,500);
+    let context = renderer.getContext();
+    let stave = new Stave(10, 40, 400);
+    stave.addClef("treble").addTimeSignature("4/4");
+    stave.setContext(context).draw();
+
+    var notes = [
+      new StaveNote({clef: "treble", keys: ["c/5"], duration: "q" }),
       new StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
       new StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
       new StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
     ];
-
-    const voice = new Voice({num_beats: 4,  beat_value: 4});
-    voice.addTickables(notes);
-
-    const formatter = new Formatter().joinVoices([voice]).formatToStave([voice], stave);
-    voice.draw(context, stave);
+    
+    var notes2 = [
+      new StaveNote({clef: "treble", keys: ["c/4"], duration: "w" })
+    ];
+    
+    // Create a voice in 4/4 and add above notes
+    var voices = [
+      new Voice({num_beats: 4,  beat_value: 4}).addTickables(notes),
+      new Voice({num_beats: 4,  beat_value: 4}).addTickables(notes2)]
+    
+    // Format and justify the notes to 400 pixels.
+    var formatter = new Formatter().joinVoices(voices).format(voices, 400);
+    
+    // Render voices
+    voices.forEach(function(v) { v.draw(context, stave); })
   }
   render() {
-    const context = new ReactNativeSVGContext(NotoFontPack, { width: 400, height: 400 });
-    this.runVexFlowCode(context);
-
     return(
       <div id="musicScore">
-          { context.render() }
       </div>
     )
   }
