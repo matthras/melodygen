64d90f1f60022f1b2353099d8d2770ee011ddb2d
Matthew Mack <matthew.y.p.mack@gmail.com>
2017-11-24 12:54:45 +1100

    added more bindings and fixed generateNewScore()

--- client/src/App.js
+++ client/src/App.js
@@ -7,34 +7,33 @@ import MusicScore from './components/MusicScore.js';
 class App extends Component {
   constructor(props) {
     super(props) 
+    this.state = {
+      users: [ ],
+      anacrusis: false,
+      clef: "treble",
+      nPitches: 4,
+      nPitchClasses: 3,
+      pitchClasses: ['C','D','E', 'F', 'G', 'A', 'B'],
+      noteLengthClasses: [1, 2, 4, 8, 16], // Whole note, minim, crotchet, quaver, semiquaver.
+      markovChain: [
+        [0.4, 0.2, 0.4],
+        [0.5, 0.3, 0.2],
+        [0.1, 0.4, 0.5]
+      ],
+      noteSequence: ['C','D','E','F'],
+      nBeats: 4,
+      beatValue: 4
+    }
     
     this.generateNewScore = this.generateNewScore.bind(this);
+    this.getNextPitch = this.getNextPitch.bind(this);
+    this.startingPitch = this.startingPitch.bind(this);
   }
-  state = {
-    users: [ ],
-    anacrusis: false,
-    clef: "treble",
-    nPitches: 4,
-    nPitchClasses: 3,
-    pitchClasses: ['C','D','E', 'F', 'G', 'A', 'B'],
-    noteLengthClasses: [1, 2, 4, 8, 16], // Whole note, minim, crotchet, quaver, semiquaver.
-    markovChain: [
-      [0.4, 0.2, 0.4],
-      [0.5, 0.3, 0.2],
-      [0.1, 0.4, 0.5]
-    ],
-    noteSequence: ['C','D','E','F'],
-    nBeats: 4,
-    beatValue: 4
-  }
+  
 
   startingPitch() {
     // If anacrusis, return random roll between 4th degree or 7th degree, else start with tonic.
-    if(this.state.anacrusis) {
-
-    } else {
-      
-    }
+    return this.state.anacrusis ? 'G' : 'C'
   }
 
   getNextPitch(currentPitch) {
@@ -50,15 +49,23 @@ class App extends Component {
     }
     return "Error" // How to do error handling here?
   }
+  
   generateNewScore() {
-    
+    let newNoteSequence = [];
+    newNoteSequence.push(this.startingPitch());
+    let currentPitch = 0;
+    for(let n = 1; n < this.state.nPitches; n++){
+      currentPitch = this.getNextPitch(currentPitch);
+      newNoteSequence.push(this.state.pitchClasses[currentPitch]);
+    }
+    this.setState({noteSequence: newNoteSequence});
   }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <button onClick={this.generateNewScore()}>
+        <button onClick={this.generateNewScore}>
           Generate Music!
         </button>
         <MusicScore
