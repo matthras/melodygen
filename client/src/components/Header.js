34267322725487f88a8e721cf777d861931d766a
Matthew Mack <matthew.y.p.mack@gmail.com>
2017-11-29 08:39:06 +1100

    added button links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return(
-            <div id="header">
-                <h1>MelodyGen</h1>
-                <div id="menu">
-                    <ul>
-                        <li>About</li>
-                        <li>Blog</li>
-                        <li>Github</li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return(
+      <div id="header">
+        <h1>MelodyGen</h1>
+        <div id="menu">
+          <a className="button primary" href="https://matthras.com/blog/post/melodygen-intro" target="_blank" rel="noopener noreferrer">About</a>
+          <a className="button primary" href="https://matthras.com/blog/melodygen" target="_blank" rel="noopener noreferrer">Blog</a>
+          <a className="button primary" href="https://github.com/matthras/melodygen" target="_blank" rel="noopener noreferrer">Github</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
