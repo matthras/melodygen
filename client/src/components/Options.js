354bff2dca929176e3aa24d96f77db1a7ee17a6d
Matthew Mack <matthew.y.p.mack@gmail.com>
2017-12-01 11:59:49 +1100

    added functioning table to render Markov chain

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -1,6 +1,28 @@
 // Component holding all the options that users can tweak to modify the settings for the score.
 import React, { Component } from 'react';
 
+function MarkovChainRow(props) {
+  const columns = props.matrixRow.map( (cell) =>
+    <td>{cell}</td>
+  );
+  return (
+    <tr>{columns}</tr>
+  );
+}
+
+function MarkovChain(props) {
+  const matrixRows = props.matrix.map( (row) =>
+    <MarkovChainRow matrixRow={row} />
+  )
+  return (
+    <table>
+      <tbody>
+      {matrixRows}
+      </tbody>
+    </table>
+  )  
+}
+
 class Options extends Component {
   constructor(props){
     super(props);
@@ -22,16 +44,17 @@ class Options extends Component {
   }
 
   render() {
-      return(
-          <div>
-              Number of Bars: <input type="number" value={this.props.nBars} onChange={this.handlenBarsChange} /> <br />
-              Time Signature: <br />
-              Number of Beats: <input type="number" value={this.props.nBeats} onChange={this.handlenBeatsChange} /> <br />
-              Beat Value: <select value={this.props.beatValue} onChange={this.handlebeatValueChange}>
-                <option value="4">4</option>
-              </select>
-          </div>
-      )
+    return(
+      <div>
+          Number of Bars: <input type="number" value={this.props.nBars} onChange={this.handlenBarsChange} /> <br />
+          Time Signature: <br />
+          Number of Beats: <input type="number" value={this.props.nBeats} onChange={this.handlenBeatsChange} /> <br />
+          Beat Value: <select value={this.props.beatValue} onChange={this.handlebeatValueChange}>
+            <option value="4">4</option>
+          </select> <br />
+          <MarkovChain matrix={this.props.markovChain} />
+      </div>
+    )
   } // beatValue additional options e.g. 2, 8, 16, etc. to be added later
         
 }
