2998aec3ef377011cc6854ba8c8b59993e5bff84
Matthew Mack <matthew.y.p.mack@gmail.com>
2017-12-04 11:22:00 +1100

    refactored options panel and added key signature option

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -24,24 +24,36 @@ class Options extends Component {
 
   render() {
     return(
+      <div id="Options">
       <div className="row">
-        <div className="five columns">
-          Number of Bars: <input type="number" value={this.props.nBars} onChange={this.handlenBarsChange} /> <br />
-          Time Signature: <br />
+        <div className="four columns">
+          <b>Number of Bars</b> <br /> <input type="number" value={this.props.nBars} onChange={this.handlenBarsChange} /> <br />
+        </div>
+        <div className="four columns">
+          <b>Time Signature</b> <br />
           Number of Beats: <input type="number" value={this.props.nBeats} onChange={this.handlenBeatsChange} /> <br />
           Beat Value: <select value={this.props.beatValue} onChange={this.handlebeatValueChange}>
             <option value="4">4</option>
           </select>
         </div>
-        <div className="seven columns">
+        <div className="four columns">
+          <b>Key Signature</b> <br />
+          <select value={this.props.keySignature} onChange={this.handlekeySignatureChange}>
+            <option value="Cmaj">C Major</option>
+          </select>
+        </div>
+      </div>
+      <div className="row">
+        <div className="twelve columns">
           Markov Chain Instructions: <br />
           <ul>
             <li>All entries should be between 0 and 1.</li>
             <li>Each row must add up to 1.</li>
           </ul>
-          In future there will be checks in place to ensure any edited Markov Chian adheres to the above rules.
+          In future there will be checks in place to ensure any edited Markov Chain adheres to the above rules.
           <MarkovChain matrix={this.props.markovChain} pitchClasses={this.props.pitchClasses} />
         </div>
+      </div>
       </div>
     )
   } // beatValue additional options e.g. 2, 8, 16, etc. to be added later
