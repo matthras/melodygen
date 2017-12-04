1728cb06b292f0388a20f9cbf52fa63584a23acf
Matthew Mack <matthew.y.p.mack@gmail.com>
2017-12-04 11:07:35 +1100

    added keys to Markov Chain table entries

--- client/src/components/MarkovChain.js
+++ client/src/components/MarkovChain.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function MarkovChainRow(props) {
-  const columns = props.matrixRow.map( (cell) =>
-    <td>{cell}</td>
+  const columns = props.matrixRow.map( (cell, index) =>
+    <td key={props.rowIndex+","+(index-1)}>{cell}</td>
   );
   return (
     <tr>{columns}</tr>
@@ -10,14 +10,14 @@ function MarkovChainRow(props) {
 }
 
 export function MarkovChain(props) {
-  const topLabels = props.pitchClasses.map( (label) => 
-    <td>{label}</td> 
+  const topLabels = props.pitchClasses.map( (label, index) => 
+    <td key={"label"+index}>{label}</td> 
   );
   // Concatenates the pitch class labels to the left of the Markov chain so that they're included in the table.
   const includeLeftLabels = props.matrix.map( (row,index) => [props.pitchClasses[index]].concat(row)
   )
-  const matrixRows = includeLeftLabels.map( (row) =>
-    <MarkovChainRow matrixRow={row} />
+  const matrixRows = includeLeftLabels.map( (row,index) =>
+    <MarkovChainRow matrixRow={row} rowIndex={index} />
   );
   return (
     <table>
