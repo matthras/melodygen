db6dc5783515221f6c63af9ad7c4be9ba421b91c
Matthew Mack <matthew.y.p.mack@gmail.com>
2017-12-04 11:52:22 +1100

    added input fields to markov chain table

--- client/src/components/MarkovChain.js
+++ client/src/components/MarkovChain.js
@@ -3,7 +3,11 @@ import React from 'react';
 function MarkovChainRow(props) {
   // Key indexes of each cell are set up so that when any value of the Markov Chain is changed, the key references the correct entry and no further number wrangling is needed. 
   const columns = props.matrixRow.map( (cell, index) =>
-    <td key={props.rowIndex+","+(index-1)}>{cell}</td>
+    (index===0) 
+      ? <td key={props.rowIndex+","+(index-1)}>{cell}</td> 
+      : <td>
+          <input className="markovchainInput" type="number" key={props.rowIndex+","+(index-1)} value={cell}></input>
+        </td>
   );
   return (
     <tr>{columns}</tr>
